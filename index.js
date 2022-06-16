076d7c1a93ac39b9ae7c9086c4c5f326a0b08bb7
Ezra-code <njerukithinji872@gmail.com>
2022-06-17 00:57:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord = word.split("").reverse().join("")
+  if(word == reverseWord){
+    return true
+  }
+  return false
 }
+console.log(isPalindrome("chairman"))
 
 /* 
   Add your pseudocode here
+  function takes one arguemnt
+  if arguement is palindrome return true else false
 */
 
 /*
   Add written explanation of your solution here
+  first i declare a variablle called reverse word 
+  it takes in the arguement passed splits it then reverses it 
+  after reversing it joins the arguement together and compares it to the original argument
+  if they match or are equal then it returns true else it returns false
 */
 
 // You can run `node index.js` to view these console logs
